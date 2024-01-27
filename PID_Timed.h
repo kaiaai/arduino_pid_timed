185ad27786ec0800e0a2de9798777fc7fbd10e77
Maker's Pet <iliao@makerspet.com>
2024-01-26 16:58:33 -0800

    Neat-up

--- PID_Timed.h
+++ PID_Timed.h
@@ -1,83 +1,83 @@
 #pragma once
-#define LIBRARY_VERSION	1.1.1
+#include <Arduino.h>
 
 class PID
 {
+public:
 
-
-  public:
-
-  //Constants used in some of the functions below
-  //#define DIRECT  0
-  //#define REVERSE  1
-  //#define P_ON_M 0
-  //#define P_ON_E 1
 	static const int8_t DIRECT = 0;
 	static const int8_t REVERSE = 1;
 	static const int8_t P_ON_M = 0;
 	static const int8_t P_ON_E = 1;
 
-  //commonly used functions **************************************************************************
-    PID(double*, double*, double*,        // * constructor.  links the PID to the Input, Output, and 
-        double, double, double, double, int, int);//   Setpoint.  Initial tuning parameters are also set here.
-                                          //   (overload for specifying proportional mode)
+  // Constructor. Links the PID to the Input, Output, and 
+  // Setpoint. Initial tuning parameters are also set here.
+  // (overload for specifying proportional mode)
+  PID(double*, double*, double*, double, double, double, double, int, int);
 
-    PID(double*, double*, double*,        // * constructor.  links the PID to the Input, Output, and 
-        double, double, double, double, int);     //   Setpoint.  Initial tuning parameters are also set here
+  // Constructor. Links the PID to the Input, Output, and
+  // Setpoint. Initial tuning parameters are also set here.
+  PID(double*, double*, double*, double, double, double, double, int);
+
+  // true Auto, false Manual
+  void enable(bool en);
+
+  // Perform the PID calculation. It should be called every time loop() cycles
+  bool Compute(double SampleTimeSec);
+
+  // Clamp output to a specific range. 0-255 by default, but
+  // it's likely the user will want to change this depending on the application
+  void SetOutputLimits(double, double);
+
+  // While most users will set the tunings once in the 
+  // constructor, this function gives the user the option
+  // of changing tunings during runtime for Adaptive control
+  void SetTunings(double, double, double);
+
+  // Overload for specifying proportional mode
+  void SetTunings(double, double, double, int);         	  
+
+  // Set the Direction, or "Action" of the controller. DIRECT
+  // means the output will increase when error is positive. REVERSE
+  // means the opposite.  it's very unlikely that this will be needed
+  // once it is set in the constructor.
+  void SetControllerDirection(int);
+
+  // Sets the frequency, in Milliseconds, with which 
+  // the PID calculation is performed. Default is 100.
+  void SetReferenceSampleTime(double);
+
+  // Zero out integral of error
+  void clearErrorIntegral();
+
+  double GetKp();
+  double GetKi();
+  double GetKd();
+  bool isEnabled(); // Pause, unpause calculations
+  int GetDirection();
+
+private:
+  void Initialize();
 	
-    void enable(bool en);                 // * true Auto, false Manual
-
-    bool Compute(double SampleTimeSec);   // * performs the PID calculation.  it should be
-                                          //   called every time loop() cycles
-
-    void SetOutputLimits(double, double); // * clamps the output to a specific range. 0-255 by default, but
-					 //   it's likely the user will want to change this depending on
-					//   the application
-	
-
-
-  //available but not commonly used functions ********************************************************
-    void SetTunings(double, double,       // * While most users will set the tunings once in the 
-                    double);         	    //   constructor, this function gives the user the option
-                                          //   of changing tunings during runtime for Adaptive control
-    void SetTunings(double, double,       // * overload for specifying proportional mode
-                    double, int);         	  
-
-	void SetControllerDirection(int);	  // * Sets the Direction, or "Action" of the controller. DIRECT
-						//   means the output will increase when error is positive. REVERSE
-						//   means the opposite.  it's very unlikely that this will be needed
-						//   once it is set in the constructor.
-  void SetReferenceSampleTime(double);    // * sets the frequency, in Milliseconds, with which 
-                                          //   the PID calculation is performed.  default is 100
-  void clearErrorIntegral();            // zero out integral of error
-                      
-  //Display functions ****************************************************************
-	double GetKp();						  // These functions query the pid for interal values.
-	double GetKi();						  //  they were created mainly for the pid front-end,
-	double GetKd();						  // where it's important to know what is actually 
-	bool isEnabled();						  // Pause, unpause calculations
-	int GetDirection();					  //
-
-  private:
-	void Initialize();
-	
-	double dispKp;				// * we'll hold on to the tuning parameters in user-entered 
-	double dispKi;				//   format for display purposes
-	double dispKd;				//
+  double dispKp;
+  double dispKi;
+  double dispKd;
     
-	double kp;                  // * (P)roportional Tuning Parameter
-    double ki;                  // * (I)ntegral Tuning Parameter
-    double kd;                  // * (D)erivative Tuning Parameter
+  double kp; // (P)roportional Tuning Parameter
+  double ki; // (I)ntegral Tuning Parameter
+  double kd; // (D)erivative Tuning Parameter
 
 	int controllerDirection;
 	int pOn;
 
-    double *myInput;              // * Pointers to the Input, Output, and Setpoint variables
-    double *myOutput;             //   This creates a hard link between the variables and the 
-    double *mySetpoint;           //   PID, freeing the user from having to constantly tell us
-                                  //   what these values are.  with pointers we'll just know.
-			  
-	//unsigned long lastTime;
+  // Pointers to the Input, Output, and Setpoint variables
+  //   This creates a hard link between the variables and the 
+  //   PID, freeing the user from having to constantly tell us
+  //   what these values are.  with pointers we'll just know.
+  double *myInput;
+  double *myOutput;
+  double *mySetpoint;
+
 	double outputSum, lastInput;
 
 	double ReferenceSampleTime;
